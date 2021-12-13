1650ffb4dd1d00558b19d331ec7b6db4d0de5216
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-13 12:07:43 -0500

    interactive website w1 (12/13/21)

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class player(models.Model):
+    plName = models.CharField(max_length=40, default="", help_text="Your name")
--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import player
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>Escape Game</title><body><h1>Welcome traveler</h1><p id="txt1">What is your name?</p><input type="text" id="nameInput"><a href="name.html"><button type="submit"id="btn1">Enter</button></a><br><p id="txt2">There is a dog and a cat. Which one will you follow?</p><a href="dog.html"><button type="submit"id="btn2">Dog</button></a><a href="cat.html"><button type="submit"id="btn3">Cat</button></a></body></html>')
+    #return HttpResponse('<html><title>Escape Game</title><body><h1>Welcome traveler</h1><p id="txt1">What is your name?</p><input type="text" id="nameInput"><a href="name.html"><button type="submit"id="btn1">Enter</button></a><br><p id="txt2">There is a dog and a cat. Which one will you follow?</p><a href="dog.html"><button type="submit"id="btn2">Dog</button></a><a href="cat.html"><button type="submit"id="btn3">Cat</button></a></body></html>')
     #return render(request, 'home.html')
+    if request.method == 'POST':
+        thePlayer = player('', request.POST.get("plName"))
+        return render(request, 'name.html', {'thePlayer' : thePlayer})
+    else:
+        return render(request, 'home.html')
 
-def name_page(request):
-    return HttpResponse('<html><title>Name Input</title><body><h1>Thank you (name here)</h1><p id="txt2">There is a dog and a cat. Which one will you follow?</p><a href="dog.html"><button type="submit"id="btn2">Dog</button></a><a href="cat.html"><button type="submit"id="btn3">Cat</button></a></body></html>')
+#def name_page(request):
+    #return HttpResponse('<html><title>Name Input</title><body><h1>Thank you (name here)</h1><p id="txt2">There is a dog and a cat. Which one will you follow?</p><a href="dog.html"><button type="submit"id="btn2">Dog</button></a><a href="cat.html"><button type="submit"id="btn3">Cat</button></a></body></html>')
+    #return render(request, 'name.html')
 
 def dog_page(request):
-    return HttpResponse('<html><title>Dog Choice</title><body><h1>You chose the dog to follow, what next?</h1><p>CODE TO BE WRITTEN HERE</p></body></html>')
+    #return HttpResponse('<html><title>Dog Choice</title><body><h1>You chose the dog to follow, what next?</h1><p>CODE TO BE WRITTEN HERE</p></body></html>')
+    return render(request, 'dog.html')
 
 def cat_page(request):
-    return HttpResponse('<html><title>Cat Choice</title><body><h1>You chose the dog to follow, what next?</h1><p>CODE TO BE WRITTEN HERE</p></body></html>')
+    #return HttpResponse('<html><title>Cat Choice</title><body><h1>You chose the cat to follow, what next?</h1><p>CODE TO BE WRITTEN HERE</p></body></html>')
+    return render(request, 'cat.html')
