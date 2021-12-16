0c39c241ff0bb911e8e044ce299c710af9326f2f
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-15 21:09:00 -0500

    added new tests and reformatted things

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,6 @@
-from selenium import webdriver
 import unittest
+from selenium import webdriver
 
-#does the selenium
 class FunctionalTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,28 +12,34 @@ class FunctionalTest(unittest.TestCase):
     # def test_Title(self):
     #     self.assertIn('Django', self.browser.title, 'Wrong title')
     
-    #tests for w1 assn
+#TESTS FOR W1 ASSN
     def testH1(self):
         headerText = self.browser.find_element_by_tag_name("h1").text
         self.assertIn('Welcome traveler', headerText, 'Headers do not match')
-
-    def testNameInput(self):
-        #txt1 = self.browser.find_elements(By.ID('txt1'))
-        #self.assertIn('What is your name?', txt1)
-        #self.browser.find_element(By.ID("nameInput")).send_keys("Sydney")
-
-        self.browser.find_element_by_id("btn1").click()
-        self.assertIn('Name Input', self.browser.title, 'functional test - doesnt direct to right page')
     
     def testDogBtn(self):
-        self.browser.find_element_by_id("btn1").click()
+        self.browser.find_element_by_id("submit").click()
         self.browser.find_element_by_id("btn2").click()
         self.assertIn('Dog Choice', self.browser.title, 'functional test - doesnt direct to right page')
 
     def testCatBtn(self):
-        self.browser.find_element_by_id("btn1").click()
+        self.browser.find_element_by_id("submit").click()
         self.browser.find_element_by_id("btn3").click()
         self.assertIn('Cat Choice', self.browser.title, 'functional test - doesnt direct to right page')
+
+#TESTS FOR W2 ASSN 
+    def testNameInput(self):
+        txt1 = self.browser.find_element_by_id('txt1').text
+        self.assertIn('What is your name?', txt1)
+        self.browser.find_element_by_id("plName").send_keys("Sydney")
+
+        self.browser.find_element_by_id("submit").click()
+        self.assertIn('Sydney', self.browser.title, 'functional test - doesnt direct to right page')
+
+    def testNameRedir(self):
+        self.browser.find_element_by_id("submit").click()
+        pTxt = self.browser.find_element_by_tag_name("p").text
+        self.assertIn('There is a dog and a cat. Which one will you follow?', pTxt, 'functional test - doesnt direct to right page')
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page
+from lists.views import home_page, cat_page, dog_page
 from django.http import HttpRequest, request, response
 
 # Create your tests here.
 class HomePageTest(TestCase):
+#TESTS FOR W1 ASSN
     def testHomePage(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page, "test.py - Home resolves incorrectly")
@@ -16,4 +17,13 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<html><title>Escape Game</title><body><h1>Welcome traveler</h1><p id="txt1">What is your name?</p><input type="text" id="nameInput"><a href="name.html"><button type="submit"id="btn1">Enter</button></a><br><p id="txt2">There is a dog and a cat. Which one will you follow?</p><a href="dog.html"><button type="submit"id="btn2">Dog</button></a><a href="cat.html"><button type="submit"id="btn3">Cat</button></a></body></html>', html)
         self.assertTrue(html.endswith('</html>'))
+
+#TESTS FOR W2 ASSN
+    def testCatPage(self):
+        found = resolve('/cat.html')
+        self.assertEqual(found.func, cat_page, "test.py - CAT PAGE resolves incorrectly")
+
+    def testHomePage(self):
+        found = resolve('/dog.html')
+        self.assertEqual(found.func, dog_page, "test.py - DOG PAGE resolves incorrectly")
         
